135b8db592bcf6338740256c75a992f4ad3ec229
dekaseptanoralisa <dekaseptanoralisa@gmail.com>
2021-09-29 10:27:50 +0700

    Bottom Navigation

---
+++ app/src/main/java/com/example/git/HomeFragment.java
@@ -0,0 +1,16 @@
+package com.example.git;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
