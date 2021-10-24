b08b490cb3b1cfc01ba7440de9e5bce58ea3f8d3
dekaseptanoralisa <dekaseptanoralisa@gmail.com>
2021-10-24 21:54:04 +0700

    Api, Recycler View

--- app/src/main/java/com/example/git/activities/MainActivity.java
+++ app/src/main/java/com/example/git/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.git.activities;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private MostPopularMovieShowsViewModel viewModel;
     private List<MovieShow> movieShows = new ArrayList<>();
     private MovieShowsAdapter movieShowsAdapter;
+    private int currentPage = 1;
+    private int totalAvailablePages = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +38,51 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(MostPopularMovieShowsViewModel.class);
         movieShowsAdapter = new MovieShowsAdapter(movieShows);
         activityMainBinding.movieShowsRecyclerView.setAdapter(movieShowsAdapter);
+        activityMainBinding.movieShowsRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if(activityMainBinding.movieShowsRecyclerView.canScrollVertically(1)){
+                    if (currentPage <= totalAvailablePages){
+                        currentPage += 1;
+                        getMostPopularMovieShows();;
+                    }
+                }
+
+            }
+        });
         getMostPopularMovieShows();
     }
 
     private void getMostPopularMovieShows() {
-        activityMainBinding.setIsloading(true);
-        viewModel.getMostPopularMovieShows(0).observe(this, mostPopularMovieShowsResponse ->{
-            activityMainBinding.setIsloading(false);
+        toogleLoading();
+        viewModel.getMostPopularMovieShows(currentPage).observe(this, mostPopularMovieShowsResponse ->{
+            toogleLoading();
             if (mostPopularMovieShowsResponse != null){
+                totalAvailablePages = mostPopularMovieShowsResponse.getPage();
                 if (mostPopularMovieShowsResponse.getMovieShows() != null){
+                    int oldCount = movieShows.size();
                     movieShows.addAll(mostPopularMovieShowsResponse.getMovieShows());
-                    movieShowsAdapter.notifyDataSetChanged();
+                    movieShowsAdapter.notifyItemRangeInserted(oldCount, movieShows.size());
                 }
             }
         });
     }
+
+    private void toogleLoading() {
+        if (currentPage == 1){
+            if (activityMainBinding.getIsLoading() != null && activityMainBinding.getIsLoading()){
+                activityMainBinding.setIsLoading(false);
+            }else{
+                activityMainBinding.setIsLoading(true);
+            }
+        }else{
+            if (activityMainBinding.getIsLoadingMore() != null && activityMainBinding.getIsLoadingMore()){
+                activityMainBinding.setIsLoadingMore(false);
+            }else{
+                activityMainBinding.setIsLoadingMore(true);
+            }
+        }
+    }
+
 }
